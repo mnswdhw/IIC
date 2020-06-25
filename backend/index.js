a90d2093a4117b98b0157d24a62a01fa24e2574a
mnswdhw <mnswdhw@gmail.com>
2020-06-25 14:45:13 +0530

    created a .env file to secure pass and id

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app=express();
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://me:create123@cluster0-3vbyh.mongodb.net/IIC?retryWrites=true&w=majority",{useNewUrlParser:true});
+require("dotenv").config();
+mongoose.connect(`mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASS}@cluster0-3vbyh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{useNewUrlParser:true});
 
 app.listen(5000,function(){
   console.log("server is up a and running on port 5000");
